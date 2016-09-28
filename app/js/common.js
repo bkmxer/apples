fbede829a8d7515a525e8196b0ed2eeb38d5f8b9
bkmxer <ilchuk_anton@mail.ru>
2016-09-28 18:31:42 +0300

    some adaptivity
    have to decrease the image of cocktail

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,14 @@
 $(window).scroll(function() {
-
-if ($(this).scrollTop() > 10){
-    $('.fixed_head').addClass("sticky");
-  }
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {}
   else{
-    $('.fixed_head').removeClass("sticky");
+    if ($(this).scrollTop() > 10){
+        $('.fixed_head').addClass("sticky");
+      }
+      else{
+        $('.fixed_head').removeClass("sticky");
+      }
   }
+
 });
 
 
@@ -43,3 +46,31 @@ $('.order_box').hover(function(){
     $('.order_box .button').removeClass("main_button");
     $(this).find('a').addClass("main_button");
 })
+
+//Get element for the counter
+var counter = $('#counter');
+//Run count function
+count(counter, 0, 90, 3000);
+
+//Count function - element for counter, starting count number, ending count number, time in milliseconds
+function count(elem, startnum, endnum, time){
+  //Create a variable to store the current counter number and add it to the counter
+  var curnum = startnum;
+  elem.text(curnum);
+  //Work out the speed of the counter
+  var speed = time / (endnum - startnum);
+  //Create the counter animation
+  var timer = window.setInterval(function(){
+    //Test if counter has finished
+    if(curnum < endnum){
+      //Increase the counter by 1 and add it to the counter
+      curnum++;
+      elem.text(curnum);
+    }else{
+      //Stop the animation
+      clearInterval(timer);
+    }
+
+  },speed);
+
+}
